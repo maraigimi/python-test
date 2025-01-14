79fcb3d281421da1eb0312c072cd2a24b7d78840
Viktor Pristas <pristas.viktor@gmail.com>
2025-01-14 14:25:24 +0100

    set up different points

--- tests/test_greet.py
+++ tests/test_greet.py
@@ -2,14 +2,6 @@ from src.exercises import *
 from io import StringIO
 import sys
 
-def test_get_sum():
-    assert get_sum(1, 2) == 3
-    assert get_sum(-1, -2) == -3
-    assert get_sum(5, -3) == 2
-    assert get_sum(0, 0) == 0
-    assert get_sum(1000000, 2000000) == 3000000
-
-
 def test_greet():
     # Redirect standard output to capture prints
     captured_output = StringIO()
